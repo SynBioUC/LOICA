70c26aa261f2f6c0cf5191b62162d7253fd92a9a
Gonza10V <gsvidal@uc.cl>
2020-09-15 14:57:25 -0300

    new tus

--- loica/assay.py
+++ loica/assay.py
@@ -1,6 +1,59 @@
+import pandas as pd
+import numpy as np
+from . import genetic_network, metabolism
+
+
+
 class Assay:
-    def __init__(self, cell, environment):
+    def __init__(self, samples, n_measurements, interval):
         '''
-        Experiment runs a set of cells in parallel, can separate populations in conditions. Fit flapjack
+        Assay measures a set of samples in parallel at a set of timepoints
+        Connects to flapjack to generate data, and to fit parameters to data
+
         '''
-    pass
+        self.samples = samples
+        self.n_measurements = n_measurements
+        self.interval = interval
+        self.measurements = pd.DataFrame()
+
+    def run(self, substeps=10):
+        '''
+        Run the assay measuring at specified time points, with simulation time step dt
+        '''
+        #substeps = self.interval / dt
+        dt = self.interval / substeps
+        for sample_id, sample in enumerate(self.samples):
+            # Integrate models
+            for t in range(self.n_measurements):
+                for tt in range(substeps):
+                    time = t * self.interval + tt * dt
+                    sample.step(time, dt)
+                # Record measurements of fluorescence
+                signals = sample.signals
+                for name, sig in signals.items():
+                    row = {'Time': time, 'Signal': name, 'Measurement': sig * sample.biomass, 'Sample':sample_id}
+                    self.measurements = self.measurements.append(row, ignore_index=True)
+                # Record measurement of biomass
+                row = {'Time': time, 'Measurement': sample.biomass, 'Signal':'Biomass', 'Sample':sample_id}
+                self.measurements = self.measurements.append(row, ignore_index=True)
+                
+    def upload(self, 
+            flapjack, 
+            name,
+            temp,
+            description,
+            study,
+            media,
+            strain,
+            signals
+            ):
+        assay = flapjack.create('assay', name=name, study=study, temperature=temp, description=description)
+        for sample_id, sample in enumerate(samples):
+            sample = fj.create('sample',
+                                row=sample_id, col=1,
+                                media=media.id[0],
+                                strain=strain.id[0],
+                                vector=vector.id[0],
+                                assay=assay.id[0],
+                                )
+
